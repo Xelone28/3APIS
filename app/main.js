487111e2c2cc3d3cff821b377897533c1c05f4eb
Gabriel Titeux <72764917+Xelone28@users.noreply.github.com>
2023-11-13 11:33:15 +0100

    Fibonacci suite

--- app/main.js
+++ app/main.js
@@ -2,7 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('EnzBEAdddddT');
+app.get('/:nbr', (req, res) => {
+    const nbr = req.params.nbr
+    let temp = []
+    let fibo = []
+    for (let round = 0; round < nbr; round++) {
+        if (temp.length != 2) {
+            temp.push(1)
+            fibo.push(1)
+        } else {
+            fibo.push(temp[0]+temp[1]);
+            temp[0] = temp[1]
+            temp[1] = fibo[fibo.length-1]
+        }
+    }
+    res.send(fibo);
 });
-app.listen(port, () => console.log(`EnzeBEAT`))
+app.listen(port, () => {
+    console.log(`First exercise app listening on port ${port}`)
+});
+
